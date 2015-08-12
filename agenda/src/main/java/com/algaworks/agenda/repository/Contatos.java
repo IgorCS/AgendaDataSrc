c9d5b7953f03a2c33c4180704eb5a317c9d43fec
IGORCS <igorweb08@gmail.com>
2015-08-12 14:41:08 -0300

    Adicionando Bilder (Agenda -__-   Com DataSource...)

---
+++ agenda/src/main/java/com/algaworks/agenda/repository/Contatos.java
@@ -0,0 +1,30 @@
+package com.algaworks.agenda.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+
+import com.algaworks.agenda.model.Contato;
+import com.algaworks.agenda.util.jpa.Transactional;
+
+public class Contatos implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private EntityManager manager;
+
+	@Transactional
+	@Named 
+	public void adicionar(Contato contato) {
+		manager.persist(contato);
+	}
+
+	public List<Contato> todos() {
+		return manager.createQuery("from Contato order by nome", Contato.class).getResultList();
+	}
+
+}
